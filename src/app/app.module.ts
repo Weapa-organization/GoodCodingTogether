3c36e93c0ebe9d38fed71027418d5ec2e5e4f839
J. Jesús González Mairena <32520710+Jiuxe@users.noreply.github.com>
2022-09-27 18:56:21 +0200

    Routing home and image-display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { ImageDisplayComponent } from './image-display/image-display.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ImageDisplayComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
