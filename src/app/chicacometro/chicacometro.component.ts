02d1b34acd755cd327351229ab1603e8d38476a0
J. Jesús González Mairena <jiuxej@gmail.com>
2023-10-18 18:16:06 +0200

    Add chicacometro

---
+++ src/app/chicacometro/chicacometro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chicacometro',
+  templateUrl: './chicacometro.component.html',
+  styleUrls: ['./chicacometro.component.css']
+})
+export class ChicacometroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
